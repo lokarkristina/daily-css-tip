427742a709ec1202839f52a8b80f4119bf10a35d
Kristina Lokar <lokarkristina79@gmail.com>
2025-02-10 12:47:45 +0100

    🧚 Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="#" width="125" height="125" />
+
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    Main page.
+  </main>
+</template>
